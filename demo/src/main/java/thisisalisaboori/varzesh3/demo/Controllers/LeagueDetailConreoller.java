bb02842abf0bdaf7d7ac874eba54062f4109fe72
ali <sgb110@gmail.com>
2023-06-10 20:26:49 +0330

    init

--- demo/src/main/java/thisisalisaboori/varzesh3/demo/Controllers/LeagueDetailConreoller.java
+++ demo/src/main/java/thisisalisaboori/varzesh3/demo/Controllers/LeagueDetailConreoller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.websocket.server.PathParam;
+
 import thisisalisaboori.varzesh3.demo.Dto.LeagueDetailDto;
 import thisisalisaboori.varzesh3.demo.Service.LeagueDetailsAppService;
 
